2de824c04a46711be795d8cef8ea45e82656294e
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-01 22:34:08 +0500

    Post.jsx created and added in App.jsx

---
+++ src/components/Post.jsx
@@ -0,0 +1,18 @@
+const Post = () => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
