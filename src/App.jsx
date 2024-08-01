a4fa12a00276512d1096709e8e9980ec17fb626a
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-01 23:01:45 +0500

    PostList.jsx added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/sidebar";
 import CreatePost from "./components/CreatePost";
-import Post from "./components/Post";
+import PostList from "./components/PostList";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <div className="content">
         <Header></Header>
         <CreatePost></CreatePost>
-        <Post></Post>
+        <PostList></PostList>
         <Footer></Footer>
       </div>
     </div>
