34188d77a737572b3127f280c301f31d462fc9ce
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-02 17:12:39 +0500

    Updated Code

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
 
 const PostList = () => {
-   return <>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-   </>
-}
+  const { postList } = useContext(PostListData);
+
+  return (
+    <>
+    {postList.map((post) => (
+      <Post key={post.id} post={post}/>
+    ))}
+    </>
+  );
+};
 
 export default PostList;
