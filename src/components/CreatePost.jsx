863ff9f5344e2097c34aa09deb6a5cede32e76b2
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-02 11:09:28 +0500

    Updated CreatePost.jsx for Error

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -16,7 +16,7 @@ const CreatePost = () => {
         </div>
       </div>
       <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
+        <label htmlFor="exampleInputPassword1" className="form-label">
           Password
         </label>
         <input
@@ -27,7 +27,7 @@ const CreatePost = () => {
       </div>
       <div className="mb-3 form-check">
         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" for="exampleCheck1">
+        <label className="form-check-label" htmlFor="exampleCheck1">
           Check me out
         </label>
       </div>
