0cf5ecf81f32a4bbd02b2c35c5e0a9453ea23ec0
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-02 16:27:29 +0500

    DEFAULT_POST_LIST created in post-list-store.jsx component

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,9 +1,49 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({});
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  return currPostList;
+};
 
 const PostListProvider = ({ children }) => {
-  return <PostList.Provider value={[]}>{children}</PostList.Provider>;
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+
+  const deletePost = () => {};
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
 };
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Lahore",
+    body: "Hi, I am going to COMSATS University Islamabad, Lahore Campus on September 2, 2024 to start my 3rd semester of BSCS",
+    reaction: 0,
+    userId: "Syed M.Hussnain Raza",
+    tags: ["#university", "#vacations", "new_journey"],
+  },
+  {
+    id: "2",
+    title: "Going to BISE Lahore",
+    body: "Hi, I am going to BISE Lahore in August for getting the duplicate of HSSC Result Card",
+    reaction: 5,
+    userId: "Hussnain Raza",
+    tags: ["#HSSC_Result_Card", "#BISE_Lahore", "Duplicate_Result_Card"],
+  },
+];
 
 export default PostListProvider;
