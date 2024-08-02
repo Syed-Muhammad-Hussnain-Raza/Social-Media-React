1ef2bbc2ea4ba6937142a2b91f4e37f31ec7ce3c
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-02 17:16:38 +0500

    Post.jsx component now  made the dynamic using props and iterating

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,23 @@
-const Post = () => {
+import styles from "./Post.module.css";
+
+const Post = ({ post }) => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+    <div className={`card ${styles.postCard}`} style={{ width: "18rem" }}>
+      <div className={`card-body ${styles.cardBody}`}>
+        <h5 className="card-title">
+          {post.title}
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {post.reaction}
+          </span>
+        </h5>
+        <p className="card-text">{post.body}</p>
+        {post.tags.map((tag) => (
+          <span
+            className={`badge rounded-pill text-bg-primary ${styles.hashtag}`}
+          >
+            {tag}
+          </span>
+        ))}
       </div>
     </div>
   );
