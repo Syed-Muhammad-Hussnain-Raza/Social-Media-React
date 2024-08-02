03e5ee2e579d3b1aa06d0b434efb142cb96a8a78
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-02 21:39:29 +0500

    Delete button successfully added in Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,33 @@
+import { useContext } from "react";
 import styles from "./Post.module.css";
 import { MdDelete } from "react-icons/md";
-
+import { PostList } from "../store/post-list-store";
 const Post = ({ post }) => {
+  const { deletePost } = useContext(PostList);
+
   return (
     <div className={`card ${styles.postCard}`}>
       <div className={`card-body ${styles.cardBody}`}>
         <h5 className="card-title">
           {post.title}
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          <span
+            className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+            onClick={() => deletePost(post.id)}
+          >
             <MdDelete />
           </span>
         </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
           <span
+            key={tag}
             className={`badge rounded-pill text-bg-primary ${styles.hashtag}`}
           >
             {tag}
           </span>
         ))}
         <div className={`alert alert-success ${styles.reactions}`} role="alert">
-          {post.reaction}
+          This post has been reacted by <strong>{post.reaction}</strong> people.
         </div>
       </div>
     </div>
