85433506a093c02d134a56d7be37b4ecc69824be
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-03 09:53:24 +0500

    Width Updated

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,7 +2,7 @@ const Sidebar = ({ selectedTab, setSelectedTab }) => {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
-      style={{ width: "280px" }}
+      style={{ width: "200px" }}
     >
       <a
         href="/"
