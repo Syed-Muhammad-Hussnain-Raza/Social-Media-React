d81a03ba293a681fc6998c0758f02aee347f7cfc
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-04 17:08:25 +0500

    Update - spinner was added using bootstrap instead of 'Loading...'

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,5 +1,15 @@
 const LoadingSpinner = () => {
-   return <h1>Loading...</h1>
-}
+  return (
+    <div className="d-flex justify-content-center" style={{ margin: "10% 0"}}>
+      <div
+        className="spinner-border"
+        role="status"
+        style={{ width: "5rem", height: "5rem" }}
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
 
 export default LoadingSpinner;
