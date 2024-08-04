d225c110efd60ac827968a7b3efc8028ff92b8bc
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-04 09:52:07 +0500

    A dummy server using fetching considered here to demonstrate server get method

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,15 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
-  const { postList } = useContext(PostListData);
+  const { postList, addInitialPosts } = useContext(PostListData);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+      });
+  }, []);
 
   return (
     <>
-    {postList.map((post) => (
-      <Post key={post.id} post={post}/>
-    ))}
+      {postList.length === 0 && <WelcomeMessage />}
+
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </>
   );
 };
