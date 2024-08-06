d6026e2a0e566b3db5ba9428b0c7f3937e762527
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-06 22:02:58 +0500

    Use of navigate made so when post created it automatically open Home tab

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,9 +1,11 @@
 import { useRef, useContext } from "react";
 import styles from "./CreatePost.module.css";
 import { PostList } from "../store/post-list-store";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
   const { addPost } = useContext(PostList);
+  const navigate = useNavigate();
 
   const userIdElement = useRef();
   const postTitleElement = useRef();
@@ -37,7 +39,10 @@ const CreatePost = () => {
       }),
     })
       .then((res) => res.json())
-      .then((post) => addPost(post));
+      .then((post) => {
+        addPost(post);
+        navigate("/");
+      });
   };
 
   return (
