73f1ad1350910ace65f9e4dc64f04ed3b716eae1
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-06 21:56:58 +0500

    Router added in main.jsx - For providing links to different components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CreatePost from "./components/CreatePost.jsx";
+import PostList from "./components/PostList.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <PostList /> },
+      { path: "/createPost", element: <CreatePost /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </React.StrictMode>
+);
