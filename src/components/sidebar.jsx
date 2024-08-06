3e3042824bacd985daed28a44d080333afa4fda7
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-06 22:00:27 +0500

    Use of Link and to instead of anchor tag and href so that the page not will refresh while shifting tabs

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,4 +1,6 @@
-const Sidebar = ({ selectedTab, setSelectedTab }) => {
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -15,41 +17,21 @@ const Sidebar = ({ selectedTab, setSelectedTab }) => {
       </a>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li
-          className="nav-item"
-          onClick={() => {
-            setSelectedTab("Home");
-          }}
-        >
-          <a
-            href="#"
-            className={`nav-link text-white ${
-              selectedTab === "Home" && "active"
-            }`}
-            aria-current="page"
-          >
+        <li className="nav-item">
+          <Link to="/" className={`nav-link text-white`} aria-current="page">
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
             Home
-          </a>
+          </Link>
         </li>
-        <li
-          onClick={() => {
-            setSelectedTab("Create Post");
-          }}
-        >
-          <a
-            href="#"
-            className={`nav-link text-white ${
-              selectedTab === "Create Post" && "active"
-            }`}
-          >
+        <li>
+          <Link to="/createPost" className={`nav-link text-white `}>
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
             Create Post
-          </a>
+          </Link>
         </li>
       </ul>
       <hr />
