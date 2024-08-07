465118f0c5ff383d5daa5cbabba5207ef57ab28a
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-07 10:26:50 +0500

    Updated - Repo name was added in vite.config.js File

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Social-Media-React",
+  base: '/Social-Media-React',
 });
